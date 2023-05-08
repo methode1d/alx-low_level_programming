6a7f606e39b0b5973338177768b6c5d4140bb62b
Methoded <dumethode@gmail.com>
2023-05-09 01:49:45 +0200

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,38 +2,28 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - This will read a text file then prints it to POSIX stdout.
- * @filename: Pointer and name of the file.
- * @letters: Number of letters the
- *           function must read and print.
- *
- * Return: If a function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can be read and printed as well.
+ * read_textfile- This will read text file that is or was printed to STDOUT.
+ * @filename: The text file will  be read
+ * @letters: The number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	free(buffer);
-	close(o);
-
+	free(buf);
+	close(fd);
 	return (w);
 }
+
