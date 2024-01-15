3e432c10f8b5369be3ce61f51e6ce5835704393c
methode1d <methode1dl@gmail.com>
2024-01-15 13:42:26 -0800

    Final

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/methode1d/alx-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
+wget -O /tmp/win.so https://github.com/methode1d/alx-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
